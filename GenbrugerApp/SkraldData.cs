fc08524b493ee606a215cc788a5a266b72472c03
Martin Dahl Kjeldsen <10704@student.eadania.dk>
2021-05-18 15:57:37 +0200

    Add VIRKER

--- GenbrugerApp/SkraldData.cs
+++ GenbrugerApp/SkraldData.cs
@@ -9,13 +9,13 @@ namespace GenbrugerApp
     class SkraldData
     {
         public string SkraldeID { get; set; }
-        public string Kategori { get; set; }
         public string Maengde { get; set; }
-        public string Beskrivelse { get; set; }
         public string Maaleenhed { get; set; }
-        public string Tid { get; set; }
-        public string AffaldspostID { get; set; }
+        public string Kategori { get; set; }
+        public string Beskrivelse { get; set; }
         public string Ansvarlig { get; set; }
         public string CVR { get; set; }
+        public string Tid { get; set; }
+        public string AffaldspostID { get; set; }
     }
 }
