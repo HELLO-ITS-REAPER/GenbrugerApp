225dba2ab7d5d5e625633c59c82452f504582b1e
Frederik Majgaard Nielsen <10709@student.eadania.dk>
2021-05-31 12:22:41 +0100

    gæm af midlertidig ændring

--- GenbrugerApp/StatisticsPage1.xaml.cs
+++ GenbrugerApp/StatisticsPage1.xaml.cs
@@ -17,6 +17,9 @@ using LiveCharts.Wpf;
 using LiveCharts.Wpf.Charts.Base;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Globalization;
+using LiveCharts.Configurations;
+using System.Data;
 
 namespace GenbrugerApp
 {
@@ -24,8 +27,8 @@ namespace GenbrugerApp
     /// Interaction logic for StatisticsPage1.xaml
     /// </summary>
     public partial class StatisticsPage1 : Page
-    { 
-        static double myList;
+    {
+
         private string kategoriValgt;
         static int KategoriInt;
         public string KategoriValgt
@@ -38,122 +41,133 @@ namespace GenbrugerApp
         {
             InitializeComponent();
 
-           
+
             SqlViewer();
-            data();
 
         }
 
-        private async void data()
+
+        private async void SqlViewer()
         {
             await System.Threading.Tasks.Task.Delay(20);
-            Chart.Colors = new List<Color>
+            List<double> allValues = new List<double>();
+            List<DateTime> allTime = new List<DateTime>();
+
+
+
+            if (kategoriValgt == "vælg kategori")
+            {
+
+            }
+            else
+            {
+
+
+
+                switch (kategoriValgt)
+                {
+                    case "Batterier":
+                        KategoriInt = 1;
+                        break;
+                    case "Biler":
+                        KategoriInt = 2;
+                        break;
+                    case "Elektronikaffald":
+                        KategoriInt = 3;
+                        break;
+                    case "Imprægneret træ":
+                        KategoriInt = 4;
+                        break;
+                    case "Inventar":
+                        KategoriInt = 5;
+                        break;
+                    case "Organisk affald":
+                        KategoriInt = 6;
+                        break;
+                    case "Pap og papir":
+                        KategoriInt = 7;
+                        break;
+                    case "Plastemballager":
+                        KategoriInt = 8;
+                        break;
+                    case "PVC":
+                        KategoriInt = 9;
+                        break;
+                    default:
+                        break;
+                }
+                SqlConnection connection = null;
+                try
+                {
+
+                    connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringDelta"].ConnectionString);
+                    SqlCommand command = new SqlCommand("SELECT * FROM Skrald WHERE Kategori = '" + KategoriInt + "'", connection);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        allValues.Add(Convert.ToDouble(reader[1]));
+                        allTime.Add(Convert.ToDateTime(reader[7]));
+                    };
+                    connection.Close();
+
+                    for (int i = allTime.Count - 10; i < allTime.Count; ++i)
+                    {
+                        allTime.FindLast
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (connection != null) connection.Close();
+                }
+
+                Chart.Colors = new List<Color>
             {
                 Colors.YellowGreen,
                 Colors.LightSeaGreen,
                 Colors.Blue
             };
 
-
-            SeriesCollection = new SeriesCollection
+                SeriesCollection = new SeriesCollection
             {
                 new LineSeries
                 {
-                    
-                    Title = kategoriValgt,
-                    Values = new ChartValues<double> { myList }
-                },
-
+                    Values = new ChartValues<double>(allValues)
+                }
             };
+                string tid1 = "hej";
+                string tid2 = "hej1";
+                string tid3 = "hej2";
 
+                Labels = new[] {"21,4,23" , "Apr", "May" };
+                YFormatter = value => value.ToString("C");
 
-            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
-            YFormatter = value => value.ToString();
+               
 
+                //modifying any series values will also animate and update the chart
 
-
-            //modifying any series values will also animate and update the chart
-            SeriesCollection[0].Values.Add(5d);
-
-            DataContext = this;
-           
-        }
-        private async void SqlViewer()
-        {
-            await System.Threading.Tasks.Task.Delay(20);
-            switch (kategoriValgt)
-            {
-                case "Batterier":
-                    KategoriInt = 1;
-                    break;
-                case "Biler":
-                    KategoriInt = 2;
-                    break;
-                case "Elektronikaffald":
-                    KategoriInt = 3;
-                    break;
-                case "Imprægneret træ":
-                    KategoriInt = 4;
-                    break;
-                case "Inventar":
-                    KategoriInt = 5;
-                    break;
-                case "Organisk affald":
-                    KategoriInt = 6;
-                    break;
-                case "Pap og papir":
-                    KategoriInt = 7;
-                    break;
-                case "Plastemballager":
-                    KategoriInt = 8;
-                    break;
-                case "PVC":
-                    KategoriInt = 9;
-                    break;
-                default:
-                    break;
-            }
-            SqlConnection connection = null;
-            try
-            {
-                //var myList = new List<String>();
-                int i = 0;
-                connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionStringDelta"].ConnectionString);
-                SqlCommand command = new SqlCommand("SELECT * FROM Skrald WHERE Kategori = '" + KategoriInt + "'", connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-
-                    //myList.Add(Convert.ToDecimal(reader[2]));
-                    if (KategoriInt !=null)
-                    {
-                        myList = (Convert.ToDouble(reader[1]));
-                        i++;
-                    }
-                  
-                    
-                    //Maaleenhed = reader[4].ToString(),
-                    //Tid = reader[5].ToString(),
-
-                };
-                connection.Close();
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                if (connection != null) connection.Close();
+                DataContext = this;
             }
         }
+
+
         public SeriesCollection SeriesCollection { get; set; }
         public string[] Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
 
     }
 }
+
+
+
+
+
+
+
     
 
