f81e560b4655c7ed808a350e1f600041c417713e
Martin Dahl Kjeldsen <10704@student.eadania.dk>
2021-06-06 11:48:01 +0200

    Creator name

--- GenbrugerApp/LoginWindow.xaml.cs
+++ GenbrugerApp/LoginWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace GenbrugerApp
     /// <summary>
     /// Interaction logic for LoginWindow.xaml
     /// </summary>
-    public partial class LoginWindow : Window
+    public partial class LoginWindow : Window // Martin har lavet hele Login
     {
         private string brugernavn = "Delta";
         private string kodeord = "1234";
